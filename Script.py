c502231881dae0bcb524fe03de01012f815a8163
sunil9958 <72134489+sunil9958@users.noreply.github.com>
2024-03-02 21:03:19 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -8,9 +8,9 @@ class script(object):
 ★ Language: <a href=https://www.python.org>Python</a>
 ★ Library: <a href=https://pyrogram.org>Pyrogram</a>"""
 
-    MY_OWNER_TXT = """★ Name: RK Bots
-★ Username: @Rk_botowner
-★ Update Channel : @Rkbotz"""
+    MY_OWNER_TXT = """★ Name: SvFilmsX
+★ Username: @SvFilmsX_admin
+★ Update Channel : @SvFilmsX_Official"""
 
     STATUS_TXT = """🗂 Total Files: <code>{}</code>
 👤 Total Users: <code>{}</code>
